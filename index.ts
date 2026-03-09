30be782873af18b7f7a846940b5ad12cf73fc2c0
bwbl <theophile2006@gmail.com>
2026-03-09 10:19:56 +0100

    add music to the md

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import {file, write} from 'bun';
+import get from 'axios'
+
+const index = file('index.md')
+const writer = index.writer();
+const req = await get('https://t2006api-gaming.theking90000.be/')
+
+let tracks = req.data['recenttracks']['track']
+
+for (let track of tracks) {
+    writer.write('-------------\n')
+    writer.write(`${track['name']}\n`)
+    writer.write(`${track['artist']['#text']}\n`)
+    writer.write(`${track['album']['#text']}\n`)
+    writer.write('-------------\n')
+}
+
+writer.write('test')
