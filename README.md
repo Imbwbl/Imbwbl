3e502f89bc195adbdd4fd071c5f755b6a539e56b
Imbwbl <bwbl@ik.me>
2026-05-26 17:24:12 +0200

    add more information about github

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 ```
-╭───────────────────────────────────────────────╮
-│                Project README               x │
-├───────────────────────────────────────────────┤
-│            This is a README file.             │
-│   It contains information about the project   │
-│     Feel free to explore and contribute!      │
-╰───────────────────────────────────────────────╯
+╭────────────────────────────────────╮
+│               Imbwbl             x │
+├────────────────────────────────────┤
+│          Lang: TypeScript          │
+│       Updated on 26 May 2026       │
+│         Stars: 0, forks: 1         │
+│  https://github.com/Imbwbl/Imbwbl  │
+╰────────────────────────────────────╯
+```
+```
+╭──────────────────────────────────────────────╮
+│                 VXV-Vitrine                x │
+├──────────────────────────────────────────────┤
+│                  Lang: HTML                  │
+│            Updated on 05 May 2026            │
+│              Stars: 0, forks: 0              │
+│  https://github.com/latifcodess/VXV-Vitrine  │
+╰──────────────────────────────────────────────╯
+```
+```
+╭──────────────────────────────────────────╮
+│               jellyfin-nix             x │
+├──────────────────────────────────────────┤
+│                Lang: Nix                 │
+│         Updated on 21 April 2026         │
+│            Stars: 1, forks: 0            │
+│  https://github.com/Imbwbl/jellyfin-nix  │
+╰──────────────────────────────────────────╯
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use octocrab::{Page, models::Repository};
+
 struct Window {
     content: Vec<String>,
     title: Option<String>,
@@ -9,8 +11,8 @@ struct Window {
 impl Window {
     fn new(content: Vec<String>, title: Option<String>, padding: Option<usize>) -> Self {
         let padding = padding.unwrap_or(0);
-        let max_line = content.iter().map(|s| s.len()).max().unwrap_or(0);
-        let width = std::cmp::max(max_line, title.as_ref().map_or(0, |t| t.len())) + (2 * padding);
+        let max_line = content.iter().map(|s| s.chars().count()).max().unwrap_or(0);
+        let width = std::cmp::max(max_line, title.as_ref().map_or(0, |t| t.chars().count())) + (2 * padding);
         Self {
             content,
             title,
@@ -39,18 +41,47 @@ impl Window {
     }
 }
 
-fn main() -> std::io::Result<()> {
-    let window = Window::new(
-        vec![
-            "This is a README file.".to_string(),
-            "It contains information about the project".to_string(),
-            "Feel free to explore and contribute!".to_string(),
-        ],
-        Some("Project README".to_string()),
-        Some(3),
-    );
+async fn get_last_pushed_repos() -> Result<Page<Repository>, octocrab::Error> {
+    let token =
+        std::env::var("GITHUB_TOKEN").expect("Please set the GITHUB_TOKEN environment variable");
+    let octocrab = octocrab::Octocrab::builder()
+        .personal_token(token)
+        .build()?;
+    Ok(octocrab
+        .current()
+        .list_repos_for_authenticated_user()
+        .sort("pushed")
+        .direction("descending")
+        .per_page(3)
+        .send()
+        .await?)
+}
 
-    let text = window.render();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenvy::dotenv()?;
+
+    let mut text = String::new();
+    let repos = get_last_pushed_repos().await?;
+    for repo in repos.items {
+        let window = Window::new(
+            vec![
+                format!("Lang: {}", repo.language.as_ref().and_then(|v| v.as_str()).unwrap_or("unknown")),
+                format!("Updated on {}", repo.updated_at
+                    .map(|dt| dt.format("%d %B %Y").to_string())
+                    .unwrap_or_else(|| "unknown".to_string())
+                ),
+                format!(
+                    "Stars: {}, forks: {}",
+                    repo.stargazers_count.unwrap_or(0), repo.forks_count.unwrap_or(0)
+                ),
+                format!("{}", repo.html_url.unwrap()),
+            ],
+            Some(repo.name),
+            Some(2),
+        );
+        text.push_str(format!("{}\n", &window.render()).as_str());
+    }
     println!("{}", text);
     fs::write("README.md", text)?;
     Ok(())
